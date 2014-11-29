8b7961c587609f0b43914a16962dc5922d73bc16
Adam Patterson <adam.patterson0012@gmail.com>
2014-11-29 17:03:40 -0500

    Adds styling and active status on nav items

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-  var  nb = $(".nav-bar");
-      nbs = "nav-bar-scrolled";
-      hdr = $(".header").height();
+  var nb = $(".nav-bar"),
+      nbs = "nav-bar-scrolled",
+      hdr = $(".header").height(),
+ sections = $(".section"),
+ nav_height = nb.outerHeight();
 
   $(window).scroll(function() {
     if( $(this).scrollTop() > hdr ) {
@@ -9,5 +11,17 @@ $(document).ready(function() {
     } else {
       nb.removeClass(nbs);
     }
+    var cur_pos = $(this).scrollTop();
+    sections.each(function() {
+      var top = $(this).offset().top-nav_height,
+        bottom = top + $(this).outerHeight();
+
+      if(cur_pos >= top && cur_pos <= bottom) {
+        nb.find("a").removeClass("active");
+        sections.removeClass("active");
+        $(this).addClass("active");
+        nb.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+      }
+    });
   });
 });
