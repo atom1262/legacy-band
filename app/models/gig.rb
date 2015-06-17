5fb4d0ef642ab8a4efed5cbb522f82367a029e51
Adam Patterson <adam.patterson0012@gmail.com>
2015-06-17 16:36:26 -0700

    Add method to format venue name for use in google maps

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -2,4 +2,12 @@ class Gig < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode
 
+  def map_name
+    name.gsub(/\s/, '+')
+  end
+
+  def api_key
+    ENV.fetch('KEY')
+  end
+
 end
